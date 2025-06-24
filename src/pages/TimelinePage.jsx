2a6480eb2b5578b19e3ab1a94455af639ceedf20
Jc-10243296 <jeffrey.chu06@gmail.com>
2025-06-24 11:59:27 -0400

    added some timelines of achievements

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -4,7 +4,7 @@ import Timeline from '../components/Timeline';
 const TimelinePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-6">Timeline of Achievements</h1>
+      {/* <h1 className="text-4xl font-bold mb-6">Timeline of Achievements</h1> */}
       <Timeline />
     </div>
   );
